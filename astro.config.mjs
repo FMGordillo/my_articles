5f4969b8067e80b9ae4313bc0742b0c025dd5153
Facundo Martin Gordillo <me@facundogordillo.com>
2023-04-11 23:14:30 +0200

    feat: removed package-lock for pnpm-lock, removed main content for articles, added mdx and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
+import remarkToc from 'remark-toc';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), mdx({
+    remarkPlugins: [remarkToc],
+  })]
+});
