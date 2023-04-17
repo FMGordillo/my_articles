a959c40ea701edd5a2cd4141e23b7dc66393657d
Facundo Martin Gordillo <me@facundogordillo.com>
2023-04-17 20:26:44 +0200

    feat: added ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
 import remarkToc from 'remark-toc';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [tailwind(), mdx({
-    remarkPlugins: [remarkToc],
-  })]
+    remarkPlugins: [remarkToc]
+  })],
+  adapter: vercel({
+    analytics: true
+  }),
 });
