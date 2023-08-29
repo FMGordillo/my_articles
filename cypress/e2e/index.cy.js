772b855623e5e1ff25e9df8f4226e67e488ab20a
Facundo Martin Gordillo <me@facundogordillo.com>
2023-08-29 18:34:02 +0200

    fix: removed cypress because, well, i am not using it

--- cypress/e2e/index.cy.js
+++
@@ -1,6 +0,0 @@
-it("should enter articles when clicked", () => {
-  const page = cy.visit("http://localhost:3000");
-  page.get("[data-slug=why-i-dont-certificate-in-cloud]").click();
-  cy.url().should("include", "articles");
-  cy.url().should("include", "why-i-dont-certificate-in-cloud");
-});
